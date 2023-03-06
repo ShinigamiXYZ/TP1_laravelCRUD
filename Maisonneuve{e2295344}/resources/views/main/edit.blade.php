2f014b9bb884c2d1bf10664298f3bcde114cdc20
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-03-06 16:10:14 -0500

    ajout modal de base confirmation delete student

--- Maisonneuve{e2295344}/resources/views/main/edit.blade.php
+++ Maisonneuve{e2295344}/resources/views/main/edit.blade.php
@@ -45,16 +45,16 @@
 
  -->
  <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#update">
   Update
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="update" tabindex="-1" role="dialog" aria-labelledby="updateLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Confirmation de changements</h5>
+        <h5 class="modal-title" id="updateLabel">Confirmation de changements</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -83,7 +83,40 @@
                 <input type="text" class="form-control" id="id" name="id" value="{{ $student->id }}">
             </div>
             
-            <button type="submit">Delete</button>
+
+            <!-- 
+    MODAL - BOOTSTRAP - DeleteCONFIRMATION
+
+ -->
+ <!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete">
+  delete
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="deleteLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteLabel">Confirmation de changements</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Voulez-vous vraiment modifier les informations?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+ <!-- END </ MODAL - Delete CONFIRMATION -->
+
+          
         </form>
     </div>
 
